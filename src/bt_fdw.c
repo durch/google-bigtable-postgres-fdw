53242b09a09bba7c41c83e830cc82f23574e0450
Drazen Urch <github@drazenur.ch>
2017-03-16 08:47:24 +0100

    Clean up comments

--- src/bt_fdw.c
+++ src/bt_fdw.c
@@ -127,31 +127,12 @@ btFdwBeginForeignScan(ForeignScanState *node, int eflags) {
 
 static TupleTableSlot *
 btFdWIterateForeignScan(ForeignScanState *node) {
-//    btFdwExecutionState *festate = (btFdwExecutionState *) node->fdw_state;
-//    TupleTableSlot *slot = node->ss.ss_ScanTupleSlot;
-//    AttInMetadata *attInMetadata = TupleDescGetAttInMetadata(node->ss.ss_currentRelation->rd_att);
-//    HeapTuple tuple;
-//    char **values;
 
     bt_fdw_state_t *st;
     st = node->fdw_state;
 
-//    node->fdw_state = state_incr(st);
-
-//    ExecClearTuple(slot);
-
     elog(LOG, "entering function %s", __func__);
 
-//    values = (char **) palloc(sizeof(char *) * 2);
-//    values[0] = "lala";
-//
-//
-//    state_incr(st, node);
-//    tuple = BuildTupleFromCStrings(attInMetadata, values);
-
-//    ExecStoreTuple(tuple, slot, InvalidBuffer, false);
-
-//    ExecStoreVirtualTuple(slot);
     bt_fdw_iterate_foreign_scan(st, node);
     TupleTableSlot *slot = node->ss.ss_ScanTupleSlot;
     return slot;
@@ -172,32 +153,6 @@ static void
 btAddForeignUpdateTargets(Query *parsetree,
                           RangeTblEntry *target_rte,
                           Relation target_relation) {
-    /*
-     * UPDATE and DELETE operations are performed against rows previously
-     * fetched by the table-scanning functions. The FDW may need extra
-     * information, such as a row ID or the values of primary-key columns, to
-     * ensure that it can identify the exact row to update or delete. To
-     * support that, this function can add extra hidden, or "junk", target
-     * columns to the list of columns that are to be retrieved from the
-     * foreign table during an UPDATE or DELETE.
-     *
-     * To do that, add TargetEntry items to parsetree->targetList, containing
-     * expressions for the extra values to be fetched. Each such entry must be
-     * marked resjunk = true, and must have a distinct resname that will
-     * identify it at execution time. Avoid using names matching ctidN or
-     * wholerowN, as the core system can generate junk columns of these names.
-     *
-     * This function is called in the rewriter, not the planner, so the
-     * information available is a bit different from that available to the
-     * planning routines. parsetree is the parse tree for the UPDATE or DELETE
-     * command, while target_rte and target_relation describe the target
-     * foreign table.
-     *
-     * If the AddForeignUpdateTargets pointer is set to NULL, no extra target
-     * expressions are added. (This will make it impossible to implement
-     * DELETE operations, though UPDATE may still be feasible if the FDW
-     * relies on an unchanging primary key to identify rows.)
-     */
 
     elog(DEBUG1, "entering function %s", __func__);
 
@@ -209,25 +164,6 @@ btPlanForeignModify(PlannerInfo *root,
                     ModifyTable *plan,
                     Index resultRelation,
                     int subplan_index) {
-    /*
-     * Perform any additional planning actions needed for an insert, update,
-     * or delete on a foreign table. This function generates the FDW-private
-     * information that will be attached to the ModifyTable plan node that
-     * performs the update action. This private information must have the form
-     * of a List, and will be delivered to BeginForeignModify during the
-     * execution stage.
-     *
-     * root is the planner's global information about the query. plan is the
-     * ModifyTable plan node, which is complete except for the fdwPrivLists
-     * field. resultRelation identifies the target foreign table by its
-     * rangetable index. subplan_index identifies which target of the
-     * ModifyTable plan node this is, counting from zero; use this if you want
-     * to index into plan->plans or other substructure of the plan node.
-     *
-     * If the PlanForeignModify pointer is set to NULL, no additional
-     * plan-time actions are taken, and the fdw_private list delivered to
-     * BeginForeignModify will be NIL.
-     */
 
     elog(DEBUG1, "entering function %s", __func__);
 
@@ -241,39 +177,10 @@ btBeginForeignModify(ModifyTableState *mtstate,
                      List *fdw_private,
                      int subplan_index,
                      int eflags) {
-    /*
-     * Begin executing a foreign table modification operation. This routine is
-     * called during executor startup. It should perform any initialization
-     * needed prior to the actual table modifications. Subsequently,
-     * ExecForeignInsert, ExecForeignUpdate or ExecForeignDelete will be
-     * called for each tuple to be inserted, updated, or deleted.
-     *
-     * mtstate is the overall state of the ModifyTable plan node being
-     * executed; global data about the plan and execution state is available
-     * via this structure. rinfo is the ResultRelInfo struct describing the
-     * target foreign table. (The ri_FdwState field of ResultRelInfo is
-     * available for the FDW to store any private state it needs for this
-     * operation.) fdw_private contains the private data generated by
-     * PlanForeignModify, if any. subplan_index identifies which target of the
-     * ModifyTable plan node this is. eflags contains flag bits describing the
-     * executor's operating mode for this plan node.
-     *
-     * Note that when (eflags & EXEC_FLAG_EXPLAIN_ONLY) is true, this function
-     * should not perform any externally-visible actions; it should only do
-     * the minimum required to make the node state valid for
-     * ExplainForeignModify and EndForeignModify.
-     *
-     * If the BeginForeignModify pointer is set to NULL, no action is taken
-     * during executor startup.
-     */
-
-//    BlackholeFdwModifyState *modify_state =
-//            palloc0(sizeof(BlackholeFdwModifyState));
-//    rinfo->ri_FdwState = modify_state;
 
     bt_fdw_state_t *st = bt_fdw_state_from_relinfo(GetUserId(), rinfo);
 
-//    elog(LOG, "entering function %s", __func__);
+    elog(LOG, "entering function %s", __func__);
 
     if (eflags & EXEC_FLAG_EXPLAIN_ONLY) {
         return;
@@ -289,38 +196,6 @@ btExecForeignInsert(EState *estate,
                     ResultRelInfo *rinfo,
                     TupleTableSlot *slot,
                     TupleTableSlot *planSlot) {
-    /*
-     * Insert one tuple into the foreign table. estate is global execution
-     * state for the query. rinfo is the ResultRelInfo struct describing the
-     * target foreign table. slot contains the tuple to be inserted; it will
-     * match the rowtype definition of the foreign table. planSlot contains
-     * the tuple that was generated by the ModifyTable plan node's subplan; it
-     * differs from slot in possibly containing additional "junk" columns.
-     * (The planSlot is typically of little interest for INSERT cases, but is
-     * provided for completeness.)
-     *
-     * The return value is either a slot containing the data that was actually
-     * inserted (this might differ from the data supplied, for example as a
-     * result of trigger actions), or NULL if no row was actually inserted
-     * (again, typically as a result of triggers). The passed-in slot can be
-     * re-used for this purpose.
-     *
-     * The data in the returned slot is used only if the INSERT query has a
-     * RETURNING clause. Hence, the FDW could choose to optimize away
-     * returning some or all columns depending on the contents of the
-     * RETURNING clause. However, some slot must be returned to indicate
-     * success, or the query's reported rowcount will be wrong.
-     *
-     * If the ExecForeignInsert pointer is set to NULL, attempts to insert
-     * into the foreign table will fail with an error message.
-     *
-     */
-
-    /* ----
-     * BlackholeFdwModifyState *modify_state =
-     *	 (BlackholeFdwModifyState *) rinfo->ri_FdwState;
-     * ----
-     */
 
     bt_fdw_state_t *st;
     st = rinfo->ri_FdwState;
@@ -332,8 +207,6 @@ btExecForeignInsert(EState *estate,
     Datum val = slot_getattr(slot, 1, &isnull);
 
     char *res = TextDatumGetCString(val);
-
-//    text *json = cstring_to_text((char *) val);
 
     bt_fdw_exec_foreign_insert(st, slot, res);
 
@@ -348,38 +221,7 @@ btExecForeignUpdate(EState *estate,
                     ResultRelInfo *rinfo,
                     TupleTableSlot *slot,
                     TupleTableSlot *planSlot) {
-    /*
-     * Update one tuple in the foreign table. estate is global execution state
-     * for the query. rinfo is the ResultRelInfo struct describing the target
-     * foreign table. slot contains the new data for the tuple; it will match
-     * the rowtype definition of the foreign table. planSlot contains the
-     * tuple that was generated by the ModifyTable plan node's subplan; it
-     * differs from slot in possibly containing additional "junk" columns. In
-     * particular, any junk columns that were requested by
-     * AddForeignUpdateTargets will be available from this slot.
-     *
-     * The return value is either a slot containing the row as it was actually
-     * updated (this might differ from the data supplied, for example as a
-     * result of trigger actions), or NULL if no row was actually updated
-     * (again, typically as a result of triggers). The passed-in slot can be
-     * re-used for this purpose.
-     *
-     * The data in the returned slot is used only if the UPDATE query has a
-     * RETURNING clause. Hence, the FDW could choose to optimize away
-     * returning some or all columns depending on the contents of the
-     * RETURNING clause. However, some slot must be returned to indicate
-     * success, or the query's reported rowcount will be wrong.
-     *
-     * If the ExecForeignUpdate pointer is set to NULL, attempts to update the
-     * foreign table will fail with an error message.
-     *
-     */
 
-    /* ----
-     * BlackholeFdwModifyState *modify_state =
-     *	 (BlackholeFdwModifyState *) rinfo->ri_FdwState;
-     * ----
-     */
 
     elog(DEBUG1, "entering function %s", __func__);
 
@@ -389,38 +231,9 @@ btExecForeignUpdate(EState *estate,
 
 static TupleTableSlot *
 btExecForeignDelete(EState *estate,
-                    ResultRelInfo *rinfo,
+                    ReultRelInfo *rinfo,
                     TupleTableSlot *slot,
                     TupleTableSlot *planSlot) {
-    /*
-     * Delete one tuple from the foreign table. estate is global execution
-     * state for the query. rinfo is the ResultRelInfo struct describing the
-     * target foreign table. slot contains nothing useful upon call, but can
-     * be used to hold the returned tuple. planSlot contains the tuple that
-     * was generated by the ModifyTable plan node's subplan; in particular, it
-     * will carry any junk columns that were requested by
-     * AddForeignUpdateTargets. The junk column(s) must be used to identify
-     * the tuple to be deleted.
-     *
-     * The return value is either a slot containing the row that was deleted,
-     * or NULL if no row was deleted (typically as a result of triggers). The
-     * passed-in slot can be used to hold the tuple to be returned.
-     *
-     * The data in the returned slot is used only if the DELETE query has a
-     * RETURNING clause. Hence, the FDW could choose to optimize away
-     * returning some or all columns depending on the contents of the
-     * RETURNING clause. However, some slot must be returned to indicate
-     * success, or the query's reported rowcount will be wrong.
-     *
-     * If the ExecForeignDelete pointer is set to NULL, attempts to delete
-     * from the foreign table will fail with an error message.
-     */
-
-    /* ----
-     * BlackholeFdwModifyState *modify_state =
-     *	 (BlackholeFdwModifyState *) rinfo->ri_FdwState;
-     * ----
-     */
 
     elog(DEBUG1, "entering function %s", __func__);
 
@@ -431,20 +244,6 @@ btExecForeignDelete(EState *estate,
 static void
 btEndForeignModify(EState *estate,
                    ResultRelInfo *rinfo) {
-    /*
-     * End the table update and release resources. It is normally not
-     * important to release palloc'd memory, but for example open files and
-     * connections to remote servers should be cleaned up.
-     *
-     * If the EndForeignModify pointer is set to NULL, no action is taken
-     * during executor shutdown.
-     */
-
-    /* ----
-     * BlackholeFdwModifyState *modify_state =
-     *	 (BlackholeFdwModifyState *) rinfo->ri_FdwState;
-     * ----
-     */
 
     elog(DEBUG1, "entering function %s", __func__);
 
@@ -452,22 +251,6 @@ btEndForeignModify(EState *estate,
 
 static int
 btIsForeignRelUpdatable(Relation rel) {
-    /*
-     * Report which update operations the specified foreign table supports.
-     * The return value should be a bit mask of rule event numbers indicating
-     * which operations are supported by the foreign table, using the CmdType
-     * enumeration; that is, (1 << CMD_UPDATE) = 4 for UPDATE, (1 <<
-     * CMD_INSERT) = 8 for INSERT, and (1 << CMD_DELETE) = 16 for DELETE.
-     *
-     * If the IsForeignRelUpdatable pointer is set to NULL, foreign tables are
-     * assumed to be insertable, updatable, or deletable if the FDW provides
-     * ExecForeignInsert, ExecForeignUpdate, or ExecForeignDelete
-     * respectively. This function is only needed if the FDW supports some
-     * tables that are updatable and some that are not. (Even then, it's
-     * permissible to throw an error in the execution routine instead of
-     * checking in this function. However, this function is used to determine
-     * updatability for display in the information_schema views.)
-     */
 
     elog(DEBUG1, "entering function %s", __func__);
 
